f78b59afaacabcd79d81be9d76ad95c699a2d1b6
plazadevina <plazadevina@gmail.com>
2025-07-25 15:03:42 +0000

    git commit -m "Code-lab-web"

---
+++ .idx/App.css
@@ -0,0 +1,13 @@
+import './App.css';
+import SideBar from ".SideBar";
+import 
+function App () {
+    return (
+        <div className ="flex text-center text-green-500 font-bold ">
+            Hallo custumer/member
+            </p>
+            </div>
+        
+    );
+}
+export default App;
