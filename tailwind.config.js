756dd90d90f6437dd9c5ed7ed136884cf3002661
plazadevina <plazadevina@gmail.com>
2025-08-08 17:03:54 +0000

    git commit -m "Code-lab-web"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,28 @@
-return[storedValue, setValue];
-};
-const use DarkMode =() => {
-    const [enabled, setEnabled]=useLocalStorage('dark-theme');
-    const isEnabled = typeof enabledState ===
-    'undefined' && enabled;
-    useEffect(()) => }
-    const className = 'dark';
-    const bodyClass = window.document.body.classList;
-    isEnabled ? bodyclass.add(className):bodyclass.remove(className);
-},[enabled, isEnabled]);
-return [enabled, setEnabled]
-};
-export default useDarkMode; 
+const colors = require('tailwindcss/colors')
+module.exports = {
+  mode:'jit',
+  purge: [`./src/**/*.{js,jsx,ts,tsx}`, `./public/index.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {}
+    colors:{
+      bg-primary: #'202225',
+      bg-secondary: #'5865f2',
+      gray: colors.trueGray,
+      900: '#202225',
+      800: '#2f3136',
+      700: '#36393f',
+      600: '#4f545c',
+      400: '#d4d7dc',
+      300: '#e3e5e8',
+      200: '#ohedef',
+      100: ,#f2f3f5',
+    },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
 }
